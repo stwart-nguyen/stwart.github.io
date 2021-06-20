c60cdd69e0f5fb954c723c747a4ba39e2dde6218
Cuong Nguyen <cuong.nguyen@oivan.com>
2021-06-20 09:56:24 +0700

    Add daily-english

---
+++ js/daily-english.js
@@ -0,0 +1,36 @@
+function handleTodayWord(todayWordObject) {
+  if (todayWordObject.word && todayWordObject.word.length) {
+    showTodayWord(todayWordObject);
+  }
+}
+
+function convertToHash(queryString) {
+  if (queryString && queryString.length && queryString.search('=') >= 0) {
+    return JSON.parse('{"' + decodeURIComponent(queryString).replace(/\+/g, ' ').replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+  }
+  else {
+    return {};
+  }
+}
+
+function convertShallowObjectToQueryString(object) {
+  var queryString = Object.keys(object).map(function (key) {
+    return encodeURIComponent(key) + '=' + encodeURIComponent(object[key]);
+  }).join('&');
+
+  return queryString;
+}
+
+function showTodayWord(wordObject) {
+  var dailyEnglishElement = document.getElementById('daily-english');
+  
+  if (!dailyEnglishElement) return;
+
+  document.getElementById('daily-english__word').innerHTML = wordObject.word;
+
+  var googleWordAnchor = document.getElementById('search-google');
+  googleWordAnchor.href = 'https://www.google.com/search?q=' + encodeURIComponent(wordObject.word);
+  googleWordAnchor.innerHTML = 'Google it';
+
+  document.getElementById('daily-english__description').innerHTML = wordObject.description || '';
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var queryStringObject = convertToHash(location.search.substring(1));
+  if (queryStringObject.word && queryStringObject.word.length) {
+    showTodayWord(queryStringObject);
+  }
+  else {
+    var todayWordAppUrl = 'https://script.google.com/macros/s/AKfycbwabFSQZITkEfwhXg1yjrWmuuVuF3yDuSNYdM7SilsNWp-ZJVjSPZ41e-Ar9GLwNjx3/exec';
+    var s = document.createElement('script');
+    s.src = todayWordAppUrl;
+    document.body.appendChild(s);
+  }
 });
