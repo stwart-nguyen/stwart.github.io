d347eae8b6ee022190800d5ba8600139e8b84b01
Cuong Nguyen <cuong.nguyen@oivan.com>
2021-06-20 11:21:15 +0700

    Show daily-english in home page only

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-  setTimeout(function () {
-    var todayWordAppUrl = 'https://script.google.com/macros/s/AKfycbwabFSQZITkEfwhXg1yjrWmuuVuF3yDuSNYdM7SilsNWp-ZJVjSPZ41e-Ar9GLwNjx3/exec';
-    var s = document.createElement('script');
-    s.src = todayWordAppUrl;
-    document.body.appendChild(s);
-  }, 500) // It wont affect at all but will remove the annoying loading status
+  if ($('#daily-english').length > 0) {
+    setTimeout(function () {
+      var todayWordAppUrl = 'https://script.google.com/macros/s/AKfycbwabFSQZITkEfwhXg1yjrWmuuVuF3yDuSNYdM7SilsNWp-ZJVjSPZ41e-Ar9GLwNjx3/exec';
+      var s = document.createElement('script');
+      s.src = todayWordAppUrl;
+      document.body.appendChild(s);
+    }, 500); // It wont affect at all but will remove the annoying loading status
+  }
 });
